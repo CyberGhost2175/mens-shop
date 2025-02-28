1636af1b01d539647f0b56631a9ba6db6c3cc249
CyberGhost2175 <asanali.itstep@mail.ru>
2025-03-01 00:01:55 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <Button>Кнопка</Button>
+            </main>
+
+        </div>
+    );
+}
